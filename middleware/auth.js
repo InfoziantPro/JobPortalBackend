a185df4f6c6f407a934b7484187551be48a6caf8
Predeep <predeepkumar.us2022cse@sece.ac.in>
2025-05-29 17:32:30 +0530

    User model name changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/usermodel');
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const authUser = async (req, res, next) => {
